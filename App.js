5bd8c7b9dbe3b69ec00747586a5832bb1bfb95e3
mart8ins <mart8ins@gmail.com>
2022-01-19 16:06:38 +0200

    initial commit, created navigation

--- App.js
+++ App.js
@@ -1,20 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+
+// NAVIGATIONS
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+// ICONS
+import { Ionicons } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+
+// COMPONENTS
+import { ShoppingListScreen } from "./src/components/shoppingList/Shopping-list.screen";
+import { RecipesScreen } from "./src/components/recipes/Recipes.screen";
+import { HomeScreen } from "./src/components/home/Home.screen";
+
+import { SafeAreaView } from "react-native";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    return (
+        <>
+            <SafeAreaView style={{ flex: 1 }}>
+                <NavigationContainer>
+                    <Tab.Navigator
+                        initialRouteName="Home"
+                        screenOptions={{
+                            tabBarActiveTintColor: "#B0B22B",
+                            headerShown: false,
+                        }}
+                    >
+                        <Tab.Screen
+                            name="Home"
+                            component={HomeScreen}
+                            options={{
+                                tabBarIcon: ({ color, size }) => (
+                                    <Ionicons
+                                        name="ios-home"
+                                        size={size}
+                                        color={color}
+                                    />
+                                ),
+                            }}
+                        />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+                        <Tab.Screen
+                            name="Shopping list"
+                            component={ShoppingListScreen}
+                            options={{
+                                tabBarIcon: ({ color, size }) => (
+                                    <FontAwesome
+                                        name="list-ul"
+                                        size={size}
+                                        color={color}
+                                    />
+                                ),
+                            }}
+                        />
+                        <Tab.Screen
+                            name="Recipes"
+                            component={RecipesScreen}
+                            options={{
+                                tabBarIcon: ({ color, size }) => (
+                                    <FontAwesome
+                                        name="book"
+                                        size={size}
+                                        color={color}
+                                    />
+                                ),
+                            }}
+                        />
+                    </Tab.Navigator>
+                </NavigationContainer>
+            </SafeAreaView>
+            <StatusBar status="auto" />
+        </>
+    );
+}
---
+++ src/components/recipes/Recipes.screen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export const RecipesScreen = () => {
+    return (
+        <View>
+            <Text>Recipes</Text>
+        </View>
+    );
+};
